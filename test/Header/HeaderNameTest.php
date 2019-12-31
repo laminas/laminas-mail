c0819a557d8652acf44bbad8639ab38d23c21c4c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:22:47 -0600

    Rewriting as Laminas Project package

--- test/Header/HeaderNameTest.php
+++ test/Header/HeaderNameTest.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mail for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mail/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mail/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Mail\Header;
+namespace LaminasTest\Mail\Header;
 
+use Laminas\Mail\Header\HeaderName;
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\Mail\Header\HeaderName;
 
 class HeaderNameTest extends TestCase
 {
@@ -77,7 +76,7 @@ class HeaderNameTest extends TestCase
      */
     public function testAssertValidRaisesExceptionForInvalidNames($name)
     {
-        $this->setExpectedException('Zend\Mail\Header\Exception\RuntimeException', 'Invalid');
+        $this->setExpectedException('Laminas\Mail\Header\Exception\RuntimeException', 'Invalid');
         HeaderName::assertValid($name);
     }
 }
