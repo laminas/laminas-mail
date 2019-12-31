d440cc0e766c72522b0f42687a1cd3a84245ca95
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:22:57 -0600

    Rewriting as Laminas Project package

--- src/Transport/Exception/DomainException.php
+++ src/Transport/Exception/DomainException.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mail for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mail/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mail/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Mail\Transport\Exception;
+namespace Laminas\Mail\Transport\Exception;
 
-use Zend\Mail\Exception;
+use Laminas\Mail\Exception;
 
 /**
- * Exception for Zend\Mail\Transport component.
+ * Exception for Laminas\Mail\Transport component.
  */
 class DomainException extends Exception\DomainException implements ExceptionInterface
 {
