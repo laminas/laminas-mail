c3686215636bf3a9a0c267de227c38b9a196d387
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:22:20 -0600

    Rewriting as Laminas Project package

--- test/Transport/SmtpTest.php
+++ test/Transport/SmtpTest.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mail for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mail/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mail/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Mail\Transport;
+namespace LaminasTest\Mail\Transport;
 
-use Zend\Mail\Headers;
-use Zend\Mail\Message;
-use Zend\Mail\Transport\Smtp;
-use Zend\Mail\Transport\SmtpOptions;
-use Zend\Mail\Transport\Envelope;
-use ZendTest\Mail\TestAsset\SmtpProtocolSpy;
+use Laminas\Mail\Headers;
+use Laminas\Mail\Message;
+use Laminas\Mail\Transport\Envelope;
+use Laminas\Mail\Transport\Smtp;
+use Laminas\Mail\Transport\SmtpOptions;
+use LaminasTest\Mail\TestAsset\SmtpProtocolSpy;
 
 /**
- * @group      Zend_Mail
+ * @group      Laminas_Mail
  */
 class SmtpTest extends \PHPUnit_Framework_TestCase
 {
@@ -36,15 +35,15 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
     public function getMessage()
     {
         $message = new Message();
-        $message->addTo('zf-devteam@zend.com', 'ZF DevTeam')
+        $message->addTo('api-tools-devteam@zend.com', 'Laminas DevTeam')
                 ->addCc('matthew@zend.com')
-                ->addBcc('zf-crteam@lists.zend.com', 'CR-Team, ZF Project')
+                ->addBcc('api-tools-crteam@lists.zend.com', 'CR-Team, Laminas Project')
                 ->addFrom(array(
-                    'zf-devteam@zend.com',
+                    'api-tools-devteam@zend.com',
                     'matthew@zend.com' => 'Matthew',
                 ))
                 ->setSender('ralph.schindler@zend.com', 'Ralph Schindler')
-                ->setSubject('Testing Zend\Mail\Transport\Sendmail')
+                ->setSubject('Testing Laminas\Mail\Transport\Sendmail')
                 ->setBody('This is only a test.');
         $message->getHeaders()->addHeaders(array(
             'X-Foo-Bar' => 'Matthew',
@@ -58,7 +57,7 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
     public function testSendMailWithoutMinimalHeaders()
     {
         $this->setExpectedException(
-            'Zend\Mail\Transport\Exception\RuntimeException',
+            'Laminas\Mail\Transport\Exception\RuntimeException',
             'transport expects either a Sender or at least one From address in the Message; none provided'
         );
         $message = new Message();
@@ -72,7 +71,7 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
     public function testSendMailWithoutRecipient()
     {
         $this->setExpectedException(
-            'Zend\Mail\Transport\Exception\RuntimeException',
+            'Laminas\Mail\Transport\Exception\RuntimeException',
             'at least one recipient if the message has at least one header or body'
         );
         $message = new Message();
@@ -92,8 +91,8 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
         $data = $this->connection->getLog();
         $this->assertContains('MAIL FROM:<mailer@lists.zend.com>', $data);
         $this->assertContains('RCPT TO:<matthew@zend.com>', $data);
-        $this->assertContains('RCPT TO:<zf-crteam@lists.zend.com>', $data);
-        $this->assertContains("From: zf-devteam@zend.com,\r\n Matthew <matthew@zend.com>\r\n", $data);
+        $this->assertContains('RCPT TO:<api-tools-crteam@lists.zend.com>', $data);
+        $this->assertContains("From: api-tools-devteam@zend.com,\r\n Matthew <matthew@zend.com>\r\n", $data);
     }
 
     public function testSendMailWithEnvelopeTo()
@@ -108,7 +107,7 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
         $data = $this->connection->getLog();
         $this->assertContains('MAIL FROM:<ralph.schindler@zend.com>', $data);
         $this->assertContains('RCPT TO:<users@lists.zend.com>', $data);
-        $this->assertContains('To: ZF DevTeam <zf-devteam@zend.com>', $data);
+        $this->assertContains('To: Laminas DevTeam <api-tools-devteam@zend.com>', $data);
     }
 
     public function testSendMailWithEnvelope()
@@ -139,11 +138,11 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
             ->setHeaders($headers)
             ->setSender('ralph.schindler@zend.com', 'Ralph Schindler')
             ->setBody('testSendMailWithoutMinimalHeaders')
-            ->addTo('zf-devteam@zend.com', 'ZF DevTeam')
+            ->addTo('api-tools-devteam@zend.com', 'Laminas DevTeam')
         ;
         $expectedMessage = "Date: Sun, 10 Jun 2012 20:07:24 +0200\r\n"
                            . "Sender: Ralph Schindler <ralph.schindler@zend.com>\r\n"
-                           . "To: ZF DevTeam <zf-devteam@zend.com>\r\n"
+                           . "To: Laminas DevTeam <api-tools-devteam@zend.com>\r\n"
                            . "\r\n"
                            . "testSendMailWithoutMinimalHeaders";
 
@@ -157,16 +156,16 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
         $message = $this->getMessage();
         $this->transport->send($message);
 
-        $expectedRecipients = array('zf-devteam@zend.com', 'matthew@zend.com', 'zf-crteam@lists.zend.com');
+        $expectedRecipients = array('api-tools-devteam@zend.com', 'matthew@zend.com', 'api-tools-crteam@lists.zend.com');
         $this->assertEquals($expectedRecipients, $this->connection->getRecipients());
 
         $data = $this->connection->getLog();
         $this->assertContains('MAIL FROM:<ralph.schindler@zend.com>', $data);
-        $this->assertContains('To: ZF DevTeam <zf-devteam@zend.com>', $data);
-        $this->assertContains('Subject: Testing Zend\Mail\Transport\Sendmail', $data);
+        $this->assertContains('To: Laminas DevTeam <api-tools-devteam@zend.com>', $data);
+        $this->assertContains('Subject: Testing Laminas\Mail\Transport\Sendmail', $data);
         $this->assertContains("Cc: matthew@zend.com\r\n", $data);
-        $this->assertNotContains("Bcc: \"CR-Team, ZF Project\" <zf-crteam@lists.zend.com>\r\n", $data);
-        $this->assertContains("From: zf-devteam@zend.com,\r\n Matthew <matthew@zend.com>\r\n", $data);
+        $this->assertNotContains("Bcc: \"CR-Team, Laminas Project\" <api-tools-crteam@lists.zend.com>\r\n", $data);
+        $this->assertContains("From: api-tools-devteam@zend.com,\r\n Matthew <matthew@zend.com>\r\n", $data);
         $this->assertContains("X-Foo-Bar: Matthew\r\n", $data);
         $this->assertContains("Sender: Ralph Schindler <ralph.schindler@zend.com>\r\n", $data);
         $this->assertContains("\r\n\r\nThis is only a test.", $data, $data);
@@ -183,7 +182,7 @@ class SmtpTest extends \PHPUnit_Framework_TestCase
             'password' => 'password',
             'host'     => 'localhost',
         ));
-        $this->assertInstanceOf('Zend\Mail\Protocol\Smtp\Auth\Login', $connection);
+        $this->assertInstanceOf('Laminas\Mail\Protocol\Smtp\Auth\Login', $connection);
         $this->assertEquals('matthew', $connection->getUsername());
         $this->assertEquals('password', $connection->getPassword());
     }
