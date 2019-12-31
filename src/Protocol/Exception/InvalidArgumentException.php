95e9b6ef0d87449b4d16d1da3b4e9e4f85addc8f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:21:19 -0600

    Rewriting as Laminas Project package

--- src/Protocol/Exception/InvalidArgumentException.php
+++ src/Protocol/Exception/InvalidArgumentException.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mail for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mail/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mail/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Mail\Protocol\Exception;
+namespace Laminas\Mail\Protocol\Exception;
 
-use Zend\Mail\Exception;
+use Laminas\Mail\Exception;
 
 /**
- * Exception for Zend_Mail component.
+ * Exception for Laminas_Mail component.
  */
 class InvalidArgumentException extends Exception\InvalidArgumentException implements
     ExceptionInterface
