6e3da97d25a26c77ab5d46429e6cefba9abf83b3
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:21:48 -0600

    Rewriting as Laminas Project package

--- src/Header/HeaderWrap.php
+++ src/Header/HeaderWrap.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mail for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mail/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mail/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Mail\Header;
+namespace Laminas\Mail\Header;
 
-use Zend\Mail\Headers;
-use Zend\Mime\Mime;
+use Laminas\Mail\Headers;
+use Laminas\Mime\Mime;
 
 /**
  * Utility class used for creating wrapped or MIME-encoded versions of header
