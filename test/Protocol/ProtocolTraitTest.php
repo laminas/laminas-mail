1ce8e791b68071e0a835a1e50eed2d474474e7bc
Elan Ruusamäe <glen@pld-linux.org>
2020-08-20 10:51:46 +0300

    Remove outdated @requires php annotation
    Signed-off-by: Elan Ruusamäe <glen@pld-linux.org>

--- test/Protocol/ProtocolTraitTest.php
+++ test/Protocol/ProtocolTraitTest.php
@@ -16,9 +16,6 @@ use PHPUnit\Framework\TestCase;
  */
 class ProtocolTraitTest extends TestCase
 {
-    /**
-     * @requires PHP 5.6.7
-     */
     public function testTls12Version()
     {
         $mock = $this->getMockForTrait(ProtocolTrait::class);
