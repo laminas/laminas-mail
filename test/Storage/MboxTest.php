8a13366e681b22d56542d158c000af0d24fa9771
Benjamin Fahl <git@fahl-design.de>
2021-08-18 19:57:47 +0200

    Fix 2 risky "This test did not perform any assertions" test errors
    Signed-off-by: Benjamin Fahl <git@fahl-design.de>

--- test/Storage/MboxTest.php
+++ test/Storage/MboxTest.php
@@ -282,6 +282,7 @@ class MboxTest extends TestCase
         } catch (\Exception $e) {
             $check = true;
             // test ok
+            $this->assertTrue($check);
         }
 
         chmod($this->mboxFile, $stat['mode']);
