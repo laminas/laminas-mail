4aa39014d20f5e27e23cfbee5e8f940368fc65f6
Christian Ebert <ebert.chris@12live.de>
2022-07-29 10:04:00 +0200

    Fixed QA issues Signed-off-by: griesi007 <noreply@12live.de>

--- test/Protocol/Xoauth2/Xoauth2Test.php
+++ test/Protocol/Xoauth2/Xoauth2Test.php
@@ -11,13 +11,13 @@ use PHPUnit\Framework\TestCase;
  */
 class Xoauth2Test extends TestCase
 {
-    public function testEncodeXouath2Sasl(){
+    public function testEncodeXouath2Sasl():void
+    {
         $this->assertEquals(
-            'dXNlcj10ZXN0QGNvbnRvc28ub25taWNyb3NvZnQuY29tAWF1dGg9QmVhcmVy'.
-            'IEV3QkFBbDNCQUFVRkZwVUFvN0ozVmUwYmpMQldaV0NjbFJDM0VvQUEBAQ==',
+            'dXNlcj10ZXN0QGNvbnRvc28ub25taWNyb3NvZnQuY29tAWF1dGg9QmVhcmVyIEV3QkFBbDNCQUFVRkZwVUFvN0ozVmUwYmpMQldaV0NjbFJDM0VvQUEBAQ==',
             Xoauth2::encodeXoauth2Sasl(
-    'test@contoso.onmicrosoft.com',
-                'EwBAAl3BAAUFFpUAo7J3Ve0bjLBWZWCclRC3EoAA'
+            'test@contoso.onmicrosoft.com',
+            'EwBAAl3BAAUFFpUAo7J3Ve0bjLBWZWCclRC3EoAA'
             )
         );
     }
