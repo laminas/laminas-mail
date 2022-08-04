9008735047e21b2ac6881e8d63674c61ab99013a
Filippo Tessarotto <zoeslam@gmail.com>
2022-08-04 16:08:49 +0200

    Update template types for plugin manager
    Signed-off-by: Filippo Tessarotto <zoeslam@gmail.com>

--- src/Protocol/SmtpPluginManagerFactory.php
+++ src/Protocol/SmtpPluginManagerFactory.php
@@ -5,19 +5,27 @@ namespace Laminas\Mail\Protocol;
 use interop\container\containerinterface;
 use Laminas\ServiceManager\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
+use Laminas\ServiceManager\ServiceManager;
 
+/**
+ * @link ServiceManager
+ *
+ * @psalm-import-type ServiceManagerConfiguration from ServiceManager
+ */
 class SmtpPluginManagerFactory implements FactoryInterface
 {
     /**
      * laminas-servicemanager v2 support for invocation options.
      *
      * @var array
+     * @psalm-var ServiceManagerConfiguration
      */
     protected $creationOptions;
 
     /**
      * {@inheritDoc}
      *
+     * @psalm-param ServiceManagerConfiguration $options
      * @return SmtpPluginManager
      */
     public function __invoke(containerinterface $container, $name, ?array $options = null)
@@ -39,6 +47,7 @@ class SmtpPluginManagerFactory implements FactoryInterface
      * laminas-servicemanager v2 support for invocation options.
      *
      * @param array $options
+     * @psalm-param ServiceManagerConfiguration $options
      * @return void
      */
     public function setCreationOptions(array $options)
