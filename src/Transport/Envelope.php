bab9ae91d1482e89b36db15c90050872c7a4d81f
Artemii Karkusha <artemii.karkusha@gmail.com>
2023-05-24 18:09:36 +0200

    Default value of propery the $to has been reverted to null.
    Signed-off-by: Artemii Karkusha <artemii.karkusha@alshaya.com>

--- src/Transport/Envelope.php
+++ src/Transport/Envelope.php
@@ -9,8 +9,8 @@ class Envelope extends AbstractOptions
     /** @var string */
     protected $from = '';
 
-    /** @var string */
-    protected $to = '';
+    /** @var string|null */
+    protected $to;
 
     /**
      * Get MAIL FROM
@@ -35,7 +35,7 @@ class Envelope extends AbstractOptions
     /**
      * Get RCPT TO
      *
-     * @return string
+     * @return string|null
      */
     public function getTo()
     {
@@ -49,6 +49,6 @@ class Envelope extends AbstractOptions
      */
     public function setTo($to)
     {
-        $this->to = (string)$to;
+        $this->to = $to;
     }
 }
