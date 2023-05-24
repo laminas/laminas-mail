314ecbb41cb31909bb9e3a04d473d23e181b512a
Artemii Karkusha <artemii.karkusha@gmail.com>
2023-05-24 16:35:47 +0200

    Suggested changes have been applied.
    Signed-off-by: Artemii Karkusha <artemii.karkusha@alshaya.com>

--- src/Transport/Sendmail.php
+++ src/Transport/Sendmail.php
@@ -9,6 +9,7 @@ use Laminas\Mail\Transport\Exception\InvalidArgumentException;
 use Laminas\Mail\Transport\Exception\RuntimeException;
 use Traversable;
 
+use function assert;
 use function count;
 use function escapeshellarg;
 use function gettype;
@@ -202,8 +203,10 @@ class Sendmail implements TransportInterface
         if (! $headers->has('subject')) {
             return '';
         }
-        $header = $headers->get('subject');
-        return $header->getFieldValue(HeaderInterface::FORMAT_ENCODED);
+        $header     = $headers->get('subject');
+        $fieldValue = $header->getFieldValue(HeaderInterface::FORMAT_ENCODED);
+        assert(is_string($fieldValue));
+        return $fieldValue;
     }
 
     /**
@@ -259,7 +262,7 @@ class Sendmail implements TransportInterface
     protected function prepareParameters(Mail\Message $message)
     {
         if ($this->isWindowsOs()) {
-            return '';
+            return;
         }
 
         $parameters = (string) $this->parameters;
