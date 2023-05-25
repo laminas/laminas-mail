89fa866ec26e6749396aabd2aacc6a5247d156fc
Artemii Karkusha <artemii.karkusha@gmail.com>
2023-05-25 14:04:10 +0200

    Revert "Issue the " InvalidNullableReturnType: The declared return type 'string' for Laminas\Mail\Transport\Envelope::getFrom is not nullable, but 'null|string' contains null (see https://psalm.dev/144)" has been fixed"
    This reverts commit d5038a6d544e27929a7da242758ee76be80da089.

--- src/Transport/Envelope.php
+++ src/Transport/Envelope.php
@@ -6,11 +6,11 @@ use Laminas\Stdlib\AbstractOptions;
 
 class Envelope extends AbstractOptions
 {
-    /** @var string */
-    protected $from =' ';
+    /** @var string|null */
+    protected $from;
 
-    /** @var string */
-    protected $to = '';
+    /** @var string|null */
+    protected $to;
 
     /**
      * Get MAIL FROM
@@ -35,7 +35,7 @@ class Envelope extends AbstractOptions
     /**
      * Get RCPT TO
      *
-     * @return string
+     * @return string|null
      */
     public function getTo()
     {
@@ -49,6 +49,6 @@ class Envelope extends AbstractOptions
      */
     public function setTo($to)
     {
-        $this->to = (string)$to;
+        $this->to = $to;
     }
 }
