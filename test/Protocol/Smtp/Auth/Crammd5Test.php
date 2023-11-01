37edbe8cab395f21cf27df10d7e73f54f1f81aa4
George Steel <george@net-glue.co.uk>
2023-11-01 21:51:59 +0000

    Remove dependency on laminas-crypt
    Instead of requiring laminas-crypt, call `hash_hmac` directly.
    
    A few psalm issues have been base-lined because we can't add native types without breaking BC. Also, some defensive conditions contradict doc-block types.
    
    Subtle potential BC break here is that an exception will now be thrown if the password is empty|null, whereas previously, it would likely have been a type error.
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/Protocol/Smtp/Auth/Crammd5Test.php
+++ test/Protocol/Smtp/Auth/Crammd5Test.php
@@ -2,6 +2,7 @@
 
 namespace LaminasTest\Mail\Protocol\Smtp\Auth;
 
+use Laminas\Mail\Exception\InvalidArgumentException;
 use Laminas\Mail\Protocol\Smtp\Auth\Crammd5;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
@@ -31,6 +32,32 @@ class Crammd5Test extends TestCase
         );
 
         $this->assertEquals('be56fa81a5671e0c62e00134180aae2c', $result);
+    }
+
+    public function testAnExceptionIsThrownForEmptyPassword(): void
+    {
+        $class  = new ReflectionClass(Crammd5::class);
+        $method = $class->getMethod('hmacMd5');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('CramMD5 authentication requires a non-empty password');
+        $method->invokeArgs(
+            $this->auth,
+            ['', 'data']
+        );
+    }
+
+    public function testAnExceptionIsThrownForEmptyChallenge(): void
+    {
+        $class  = new ReflectionClass(Crammd5::class);
+        $method = $class->getMethod('hmacMd5');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('CramMD5 authentication requires a non-empty challenge');
+        $method->invokeArgs(
+            $this->auth,
+            ['foo', '']
+        );
     }
 
     public function testUsernameAccessors(): void
